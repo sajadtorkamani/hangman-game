dbeedff2b9e6763d34b634fefd73213df75602ea
Sajad Torkamani <sajadtorkamani1@gmail.com>
2022-04-06 11:20:45 +0100

    Setup tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 import { Counter } from './features/counter/Counter'
 
 function App() {
-  return <Counter />
+  return (
+    <>
+      <h1 className="text-3xl mb-4">Hangman</h1>
+      <Counter />
+    </>
+  )
 }
 
 export default App
