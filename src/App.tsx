e5121a3239e4b309c7fbeba0e12aa66fa98e6e82
Sajad Torkamani <sajadtorkamani1@gmail.com>
2022-04-06 11:54:31 +0100

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import { Counter } from './features/counter/Counter'
 
 function App() {
   return (
     <>
       <h1 className="text-3xl mb-4">Hangman</h1>
-      <Counter />
     </>
   )
 }
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,11 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
-import counterReducer from '../features/counter/counterSlice'
+import { configureStore } from '@reduxjs/toolkit'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
   },
 })
 
-export type AppDispatch = typeof store.dispatch
+// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
->
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
