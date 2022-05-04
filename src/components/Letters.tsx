f212841e2b29a7354aff3bfc10be8867ea40ec53
Sajad Torkamani <sajadtorkamani1@gmail.com>
2022-05-04 15:11:28 +0100

    Don't let user guess with keys

--- src/components/Letters.tsx
+++ src/components/Letters.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { makeGuess } from '../store/gameSlice'
 import { lettersGuessedSelector } from '../store/gameSliceSelectors'
 import { useStoreDispatch, useStoreSelector } from '../store/hooks'
@@ -8,23 +8,6 @@ const Letters: React.FC = () => {
   const letters = getLettersArray()
   const dispatch = useStoreDispatch()
   const lettersGuessed = useStoreSelector(lettersGuessedSelector)
-
-  const handleKeyPress = ({ key: letter }: KeyboardEvent) => {
-    const isAlreadyGuessed = lettersGuessed.includes(letter)
-    
-    if (/^[A-Za-z]{1}$/.test(letter) && !isAlreadyGuessed) {
-      dispatch(makeGuess(letter))
-    }
-  }
-
-  useEffect(() => {
-    window.addEventListener('keydown', handleKeyPress)
-
-    // Stop listening for key down when component is unmounted
-    return () => {
-      window.removeEventListener('keydown', handleKeyPress)
-    }
-  })
 
   return (
     <div className="flex flex-wrap justify-center">
